ad4ccf134e7de282f9170116796a0cc485d8f384
Diah Putri Lestari <diahputrilestari6@gmail.com>
2025-06-30 20:58:32 +0700

    fix error build

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-      "./src/**/*.{js,ts,jsx,tsx}", 
+      // "./src/**/*.{js,ts,jsx,tsx}", 
+      './index.html', './src/**/*.{js,jsx,ts,tsx}'
+
   ],
   theme: {
     extend: {},
